0a340c5fb3d1e4ae36ddfe969ca6de3f50ee766e
Will Olson <will.olson.b@gmail.com>
2024-11-15 18:49:12 -0600

    Revised CSS line in index.js for direct import instead of as variable.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import routes from './Components/Routing';
-import CSS from './App.css';
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
