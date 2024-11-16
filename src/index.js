4d4baef142964f70bd9d0859630744e0fb9c5c32
Will Olson <will.olson.b@gmail.com>
2024-11-15 18:41:05 -0600

    Added CSS import back in, shifted from Home to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import routes from './Components/Routing';
+import CSS from './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
